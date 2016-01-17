a80ffd0b5d54c93c1928903c3e8309148656a8dd
Mattias Moren <mattias@burtrask.net>
2016-01-17 19:54:14 +0100

    Test

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+//    QCoreApplication a(argc, argv);
 
-    return a.exec();
+//    return a.exec();
+
+     qWarning("Hello World!");
+
 }
